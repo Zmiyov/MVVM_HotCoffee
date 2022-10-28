40814006525877da982bc38e987db31fe0f62960
Volodymyr <zmiyov88@gmail.com>
2022-10-28 17:19:07 +0300

    Add post data

--- MVVM_HotCoffee/Controllers/AddOrderViewController.swift
+++ MVVM_HotCoffee/Controllers/AddOrderViewController.swift
@@ -70,5 +70,14 @@ class AddOrderViewController: UIViewController, UITableViewDelegate, UITableView
         self.viewModel.email = email
         self.viewModel.selectedSize = selectedSize
         self.viewModel.selectedType = self.viewModel.types[indexPath.row]
+        
+        WebService().load(resource: Order.create(viewModel: self.viewModel)) { result in
+            switch result {
+            case .success(let order):
+                print(order)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
